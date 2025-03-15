68de7b2eee142b1bba37e5ac67ab1df74b9baa08
geekinryxnn <ryankiratu67@gmail.com>
2025-03-15 16:23:56 +0300

    removed image

--- SCRIPT/index.js
+++ SCRIPT/index.js
@@ -1,8 +1,38 @@
 const ramens = [
-    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "Images/gyukotsu.jpg", rating: 5, comment: "Delicious!" },
-    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "Images/kojiro (1).jpg", rating: 4, comment: "Very flavorful!" },
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "Images/naruto.jpg" },
-    { id: 4 , name:"Naruto Ramen" ,restaurant:"Kempiski" , image:"Images/nirvana.jpg" ,rating: 7, comment:"Very healthy"}
+    { 
+        id: 1, name: "Shoyu Ramen", 
+        restaurant: "Ichiran", 
+        image: "Images/gyukotsu.jpg", 
+        rating:  "5", 
+        comment: "Delicious!" ,
+    },
+
+    { 
+        id: 2, 
+        name: "Miso Ramen", 
+        restaurant: "Menya", 
+        image: "Images/kojiro (1).jpg", 
+        rating: "4" , 
+        comment: "Very flavorful!" ,
+    },
+
+    { 
+        id: 3, 
+        name: "Tonkotsu Ramen", 
+        restaurant: "Ramen-ya", 
+        image: "Images/naruto.jpg", 
+        rating:"8", 
+        comment: "Very colorful",
+    },
+
+    { 
+        id: 4 , 
+      name:"Naruto Ramen" ,
+      restaurant:"Kempiski" , 
+      image:"Images/nirvana.jpg" ,
+      rating: "7", 
+      comment:"Very healthy",
+    },
 ];
 
 function displayRamens() {
